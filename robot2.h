b134ff2504e529dda65643a2f22e97c75a9b6cd9
nidqija <ajiqdin646@gmail.com>
2025-05-21 16:52:52 +0800

    added copy constructor for each robot

--- robot2.h
+++ robot2.h
@@ -1,139 +1,97 @@
-
-    #include <string>
-    #include <cstdlib>
-    #include <vector>
-    #include <sstream>
-    #include <ctime>
-    #include <iostream>
-    #include <vector>
-
-    using namespace std;
+#include <string>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <iostream>
+using namespace std;
 
 class Robot {
-private:
-    string symbol;
-    string line;
-    
-    vector<vector<int>>robotAmount;
-    
-public:
+protected:
     int x, y;
-    Robot() {
-        x = 1; 
-        y = 1;
-        symbol = "R";
+    string symbol;
+    vector<string> robotInitialLineup;
+
+public:
+    Robot() : x(1), y(1), symbol("R") {}
+
+    Robot(const Robot& robot) {
+        this->x = robot.x;
+        this->y = robot.y;
+        this->symbol = robot.symbol;
     }
 
-      Robot(const Robot& robot) {
-        x = robot.x;
-        y = robot.y;
-        symbol = robot.symbol;
+    int getX() const {
+         return x; 
+        
     }
 
-     
-
-
-      
-
-    int getX() const { return x; }
-    int getY() const { return y; }
-    void setX(int x_) { x = x_; }
-    void setY(int y_) { y = y_; }
-    string ReturnRobot() const { return symbol; }
-
-
-    string DetectRobot(){
-      ifstream inputFile("input.txt");
-
-       while (getline(inputFile, line)) {
-            // Process only lines that contain "GenericRobot"
-            if (line.find("GenericRobot") != string::npos) {
-                size_t StartPos = line.find("GenericRobot");
-                
-                // Create stream from substring starting from "GenericRobot"
-                istringstream iss(line.substr(StartPos));
-                
-                string tag, robotName , robotInitial;
-                int x, y;
-
-
-                // Extract: tag (e.g., "GenericRobot"), name, and two coordinates
-                iss >> tag >> robotName >> x >> y;
-                robotInitial = robotName.substr(0,1);
-
-  
-                
-
-                // Print robot name (can be removed for silent loading)
-                cout << robotName << endl;
-            }
+    int getY() const {
+         return y; 
         }
 
+    void setX(int x_) {
+         x = x_; 
+        }
+
+    void setY(int y_) {
+         y = y_; 
+        }
+
+    string getSymbol() const {
+         return symbol; 
+        }
+
+    void DetectRobot() {
+        ifstream inputFile("input.txt");
+        string line;
+        robotInitialLineup.clear();
+
+        while (getline(inputFile, line)) {
+            if (line.find("GenericRobot") != string::npos) {
+                istringstream iss(line);
+                string tag, robotName;
+                int x, y;
+                iss >> tag >> robotName >> x >> y;
+                string initial = robotName.substr(0, 1);
+                robotInitialLineup.push_back(initial);
+            }
+        }
+    }
+
+    vector<string> ReturnVectorRobotInitial() const {
+        return robotInitialLineup;
     }
 };
 
+class MovingBot : public Robot {
+private:
+    int robotChoice;
+    string icon;
 
+public:
+    MovingBot() : robotChoice(0) {}
 
-    class MovingBot : public Robot{
-        private:
-        string line;
-        int robotChoice;
+    MovingBot(const MovingBot& other) : Robot(other), robotChoice(other.robotChoice), icon(other.icon) {}
 
-        public:
-
-        friend class Battlefield;
-        int randomNumbers(){
-            cout << robotChoice << endl;
-        };
-
-
-        MovingBot() : Robot(){
-            robotChoice = 0;
+    void MovetheBot() {
+        robotChoice = rand() % 8;
+        switch (robotChoice) {
+            case 0: x -= 1; break; // Left
+            case 1: x += 1; break; // Right
+            case 2: y -= 1; break; // Up
+            case 3: y += 1; break; // Down
+            case 4: x += 1; y -= 1; break; // Top-right
+            case 5: x -= 1; y -= 1; break; // Top-left
+            case 6: x += 1; y += 1; break; // Bottom-right
+            case 7: x -= 1; y += 1; break; // Bottom-left
         }
-
-
-        MovingBot(const MovingBot& other ) : Robot(other){
-            this-> robotChoice = other.robotChoice;
-        }
-
-
-
-       
-
-
-     void MovetheBot() {
-    robotChoice = rand() % 8;
-
-    switch (robotChoice) {
-        case 0: x -= 1; break;               // Left
-        case 1: x += 1; break;               // Right
-        case 2: y -= 1; break;               // Up
-        case 3: y += 1; break;               // Down
-        case 4: x += 1; y -= 1; break;       // Top-right
-        case 5: x -= 1; y -= 1; break;       // Top-left
-        case 6: x += 1; y += 1; break;       // Bottom-right
-        case 7: x -= 1; y += 1; break;       // Bottom-left
     }
-}
 
+    void setIcon(const string& icon_) {
+        icon = icon_;
+    }
 
-
-   int ReturnSetX() const{
-     return x;
-   }
-
-     int ReturnSetY() const{
-     return y;
-   }
-
-
-
-        
-    };
-
-
-
-
-
-
-
+    string getIcon() const {
+        return icon;
+    }
+};
